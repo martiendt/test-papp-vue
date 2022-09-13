81025f431871259247bef8bba75c1077fc3b2f37
Martien Dermawan Tanama <martiendt@gmail.com>
2022-09-13 09:44:02 +0700

    add tailwindcss

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,11 +3,16 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
   extends: [
     'plugin:vue/vue3-recommended',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier',
+    'plugin:tailwindcss/recommended',
   ],
   rules: {
     'prettier/prettier': ['error', { singleQuote: true, printWidth: 120, semi: false }],
--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
+    <div>
       <HelloWorld msg="Point App" />
 
       <nav>
