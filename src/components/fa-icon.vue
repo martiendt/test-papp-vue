d227400bf718ffa713bd6e0c51e26b47ba91c0e6
Martien Dermawan Tanama <martiendt@gmail.com>
2022-11-08 11:30:49 +0700

    z

--- src/components/fa-icon.vue
+++ src/components/fa-icon.vue
@@ -1,12 +1,18 @@
 <template>
-  <span class="wait-for-fa">
+  <span>
     <i :class="props.icon" :data-fa-transform="transform"></i>
   </span>
 </template>
 
 <script setup lang="ts">
 const props = defineProps({
-  icon: String,
-  transform: String,
+  icon: {
+    type: String,
+    default: '',
+  },
+  transform: {
+    type: String,
+    default: '',
+  },
 })
 </script>
--- src/layouts/app-main-sidebar.vue
+++ src/layouts/app-main-sidebar.vue
@@ -4,28 +4,16 @@
     <!-- Main Sidebar -->
     <div class="main-sidebar-shortcut">
       <div class="main-sidebar-shortcut-container">
-        <!-- Application Logo -->
         <div class="flex pt-4">
-          <a href="/">
-            <img
-              class="h-12 w-12 transition-transform duration-500 ease-in-out hover:rotate-[360deg]"
-              src="@/assets/logo-icon.png"
-              alt="logo"
-            />
-          </a>
-        </div>
-        <!-- Main Sections Links -->
-        <div class="is-scrollbar-hidden flex grow flex-col space-y-4 overflow-y-auto pt-6 text-navy-200">
-          <router-link
-            to="#"
-            class="flex h-11 w-11 items-center justify-center rounded-lg outline-none transition-colors duration-200 hover:bg-navy-300/20 focus:bg-navy-300/20 active:bg-navy-300/25"
-          >
-            <fa-icon icon="fa-solid fa-house w-6 h-6" class="" />
+          <router-link to="/">
+            <img class="main-sidebar-logo" src="@/assets/logo-icon.png" alt="logo" />
           </router-link>
-          <router-link
-            to="#"
-            class="flex h-11 w-11 items-center justify-center rounded-lg outline-none transition-colors duration-200 hover:bg-navy-300/20 focus:bg-navy-300/20 active:bg-navy-300/25"
-          >
+        </div>
+        <div class="main-sidebar-shortcut-body">
+          <router-link to="#" class="main-sidebar-shortcut-link">
+            <fa-icon icon="fa-solid fa-house w-6 h-6" />
+          </router-link>
+          <router-link to="#" class="main-sidebar-shortcut-link">
             <fa-icon icon="fa-regular fa-wand-magic-sparkles w-6 h-6" />
           </router-link>
         </div>
@@ -34,21 +22,18 @@
 
     <!-- Sidebar Panel -->
     <div class="main-sidebar-panel">
-      <div class="flex h-full grow flex-col bg-navy-750">
+      <div class="main-sidebar-panel-container">
         <!-- Sidebar Panel Header -->
-        <div class="flex h-18 w-full items-center justify-between pl-4 pr-1">
+        <div class="main-sidebar-panel-header">
           <p class="text-base tracking-wider text-navy-100">Templates</p>
           <component :is="ComponentToggleSidebar" v-if="screenBreakpointStore.windowWidth < 1024" />
         </div>
 
         <!-- Sidebar Panel Body -->
-        <div class="h-[calc(100%-4.5rem)] overflow-x-hidden pb-6">
-          <ul class="flex flex-1 flex-col px-4 font-inter">
+        <div class="main-sidebar-panel-body">
+          <ul class="flex flex-1 flex-col px-4">
             <li v-for="menu in sideMenuPanelStore.menu" :key="menu.title" class="">
-              <button
-                class="flex w-full items-center justify-between py-2 text-xs+ tracking-wide outline-none transition-[color,padding-left] duration-300 ease-in-out text-navy-200 hover:text-navy-50"
-                @click="menu.active = !menu.active"
-              >
+              <button class="menu-link-button" @click="menu.active = !menu.active">
                 <span>{{ menu.title }}</span>
                 <fa-icon
                   v-if="menu.subMenu"
@@ -59,19 +44,16 @@
               <template v-if="menu.active">
                 <ul v-for="subMenu in menu.subMenu" :key="subMenu.title" class="">
                   <li>
-                    <a
-                      href="#"
-                      class="flex items-center justify-between p-2 text-xs+ tracking-wide outline-none transition-[color,padding-left] duration-300 ease-in-out hover:pl-4 text-navy-200 hover:text-navy-50"
-                    >
+                    <router-link to="#" class="submenu-link">
                       <div class="flex items-center space-x-2">
-                        <div class="h-1.5 w-1.5 rounded-full border border-current opacity-40"></div>
+                        <div class="bullet-list"></div>
                         <span>{{ subMenu.title }}</span>
                       </div>
-                    </a>
+                    </router-link>
                   </li>
                 </ul>
               </template>
-              <div v-if="menu.separator" class="my-3 h-px bg-slate-700"></div>
+              <div v-if="menu.separator" class="menu-separator"></div>
             </li>
           </ul>
         </div>
@@ -90,6 +72,10 @@ const sideMenuPanelStore = useSideMenuPanelStore()
 </script>
 
 <style lang="postcss" scoped>
+.main-sidebar-logo {
+  @apply h-12 w-12 transition-transform duration-500 ease-in-out hover:rotate-[360deg];
+}
+
 .is-sidebar-open .main-sidebar {
   @apply fixed w-full h-full bg-black/50 z-30 lg:bg-black/0 lg:w-0 lg:h-0;
 }
@@ -98,8 +84,20 @@ const sideMenuPanelStore = useSideMenuPanelStore()
   @apply fixed w-[var(--main-sidebar-shortcut-width)] h-full z-40 -translate-x-full opacity-100 bg-slate-700;
 }
 
+.main-sidebar-shortcut-container {
+  @apply flex h-full w-full flex-col items-center border-r bg-navy-800 border-navy-700;
+}
+
+.main-sidebar-shortcut-body {
+  @apply is-scrollbar-hidden flex grow flex-col space-y-4 overflow-y-auto pt-6 text-navy-200;
+}
+
 .is-sidebar-open .main-sidebar-shortcut {
   @apply translate-x-0;
+}
+
+.main-sidebar-shortcut-link {
+  @apply flex h-11 w-11 items-center justify-center rounded-lg outline-none transition-colors duration-200 hover:bg-navy-300/20 focus:bg-navy-300/20 active:bg-navy-300/25;
 }
 
 .main-sidebar-panel {
@@ -110,7 +108,31 @@ const sideMenuPanelStore = useSideMenuPanelStore()
   @apply translate-x-0 ease-out;
 }
 
-.main-sidebar-shortcut-container {
-  @apply flex h-full w-full flex-col items-center border-r bg-navy-800 border-navy-700;
+.main-sidebar-panel-container {
+  @apply flex h-full grow flex-col bg-navy-750;
+}
+
+.main-sidebar-panel-header {
+  @apply flex h-18 w-full items-center justify-between pl-4 pr-1;
+}
+
+.main-sidebar-panel-body {
+  @apply h-[calc(100%-4.5rem)] overflow-x-hidden pb-6;
+}
+
+.main-sidebar-panel-body .menu-separator {
+  @apply my-3 h-px bg-slate-700;
+}
+
+.main-sidebar-panel-body .bullet-list {
+  @apply h-1.5 w-1.5 rounded-full border border-current opacity-40;
+}
+
+.main-sidebar-panel-body .menu-link-button {
+  @apply flex w-full items-center justify-between py-2 text-xs+ tracking-wide outline-none transition-[color,padding-left] duration-300 ease-in-out text-navy-200 hover:text-navy-50;
+}
+
+.main-sidebar-panel-body .submenu-link {
+  @apply flex items-center justify-between p-2 text-xs+ tracking-wide outline-none transition-[color,padding-left] duration-300 ease-in-out hover:pl-4 text-navy-200 hover:text-navy-50;
 }
 </style>
