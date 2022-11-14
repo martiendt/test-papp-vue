753ee1c0a8dbd4e5b54445ee7c0b6497481f1dc2
Martien Dermawan Tanama <martiendt@gmail.com>
2022-11-14 08:15:31 +0700

    ads

--- src/layouts/app-layout.vue
+++ src/layouts/app-layout.vue
@@ -5,12 +5,9 @@
   <div class="h-100vh flex grow bg-slate-50 dark:bg-slate-900">
     <app-main-sidebar class="print:hidden"></app-main-sidebar>
     <app-header class="print:hidden"></app-header>
-    <!-- <app-secondary-sidebar></app-secondary-sidebar> -->
-    <!-- <app-mobile-searchbar></app-mobile-searchbar> -->
     <main class="main-content">
       <RouterView />
     </main>
-    <!-- <app-footer></app-footer> -->
   </div>
 </template>
 
@@ -19,17 +16,4 @@ import { RouterView } from 'vue-router'
 import AppPreloader from '@/components/app-preloader.vue'
 import AppMainSidebar from './app-main-sidebar.vue'
 import AppHeader from './app-header.vue'
-// import AppSecondarySidebar from './app-secondary-sidebar.vue'
-// import AppMobileSearchbar from './app-mobile-searchbar.vue'
-// import AppFooter from './app-footer.vue'
 </script>
-
-<style lang="postcss" scoped>
-.main-content {
-  @apply w-full lg:w-full px-8;
-}
-
-.is-sidebar-open .main-content {
-  @apply w-full lg:w-[calc(100%-(var(--main-sidebar-shortcut-width)+var(--main-sidebar-panel-width)))] lg:ml-[calc(var(--main-sidebar-shortcut-width)+var(--main-sidebar-panel-width))];
-}
-</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,6 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'class',
-  theme: {
-    extend: {
-      keyframes: {
-        'fade-out': {
-          '0%': {
-            opacity: 1,
-            visibility: 'visible',
-          },
-          '100%': {
-            opacity: 0,
-            visibility: 'hidden',
-          },
-        },
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 }
